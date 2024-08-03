4a733e9bbb4fa27e1a555aa8f513a725ab2a3105
HampCode <pcsiheme@upc.edu.pe>
2024-08-03 17:50:32 -0500

    first commit

---
+++ src/main/java/com/hampcode/restaurant_reservation/service/ReservationService.java
@@ -0,0 +1,85 @@
+package com.hampcode.restaurant_reservation.service;
+
+import com.hampcode.restaurant_reservation.domain.entity.Reservation;
+import com.hampcode.restaurant_reservation.domain.entity.Restaurant;
+import com.hampcode.restaurant_reservation.domain.entity.User;
+import com.hampcode.restaurant_reservation.domain.enums.ReservationStatus;
+import com.hampcode.restaurant_reservation.dto.request.ReservationRequestDTO;
+import com.hampcode.restaurant_reservation.dto.response.ReservationResponseDTO;
+import com.hampcode.restaurant_reservation.exception.ResourceNotFoundException;
+import com.hampcode.restaurant_reservation.mapper.ReservationMapper;
+import com.hampcode.restaurant_reservation.repository.ReservationRepository;
+import com.hampcode.restaurant_reservation.repository.RestaurantRepository;
+import com.hampcode.restaurant_reservation.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class ReservationService {
+
+  private final ReservationRepository reservationRepository;
+  private final RestaurantRepository restaurantRepository;
+  private final UserRepository userRepository;
+  private final ReservationMapper reservationMapper;
+
+  @Transactional
+  public ReservationResponseDTO createReservation(ReservationRequestDTO reservationRequestDTO) {
+
+    Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+
+    User user = userRepository.findOneByEmail(authentication.getName())
+        .orElseThrow(ResourceNotFoundException::new);
+
+    Restaurant restaurant = restaurantRepository.findById(reservationRequestDTO.getRestaurantId())
+      .orElseThrow(() -> new ResourceNotFoundException("Restaurant not found"));
+
+    Reservation reservation = reservationMapper.toEntity(reservationRequestDTO);
+    reservation.setRestaurant(restaurant);
+    reservation.setClient(user);
+    reservation.setStatus(ReservationStatus.PENDING);
+    reservation.calculateTotalAmount();
+
+    reservation = reservationRepository.save(reservation);
+
+    return reservationMapper.toResponseDto(reservation);
+  }
+
+  @Transactional(readOnly = true)
+  public List<ReservationResponseDTO> getReservationsByClientId() {
+
+    Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+    User user = userRepository.findOneByEmail(authentication.getName())
+        .orElseThrow(() -> new ResourceNotFoundException("User not found"));
+
+    List<Reservation> reservations = reservationRepository.findByClientId(user.getId());
+    return reservationMapper.toResponseDtoList(reservations);
+  }
+
+  /*@Transactional
+  public ReservationResponseDTO confirmReservation(Long reservationId) {
+    Reservation reservation = reservationRepository.findById(reservationId)
+      .orElseThrow(() -> new ResourceNotFoundException("Reservation not found"));
+    reservation.setStatus(ReservationStatus.CONFIRMED);
+    reservation = reservationRepository.save(reservation);
+    return reservationMapper.toResponseDto(reservation);
+  }*/
+
+  @Transactional
+  public Reservation confirmReservationPayment(Long reservationId) {
+    Reservation reservation = reservationRepository
+            .findById(reservationId)
+            .orElseThrow(ResourceNotFoundException::new);
+
+    reservation.setStatus(ReservationStatus.PAID);
+    return reservationRepository.save(reservation);
+  }
+
+
+
+}
